9061dc24fc2a49a6b3402fe8d76326f3819960f2
hugo <hugo.varloud@gmail.com>
2024-03-12 12:28:13 +0100

    login registrer requests from interface

--- todo-list-front/src/views/Register.jsx
+++ todo-list-front/src/views/Register.jsx
@@ -1,8 +1,23 @@
 import { Box, Button, TextField, Checkbox, Grid } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const handleSubmit = (event) => {
-      console.log("HANDLE REGISTER : ");
+        event.preventDefault();
+        axios.post('http://localhost:3000/register', {username, password})
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+    }
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
     }
     return (
         <>
@@ -14,39 +29,43 @@ export default function Login() {
                 alignItems: 'center',
             }}
             >
-            <h1 component="h1" variant="h5">
-                S'inscire
-            </h1>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                />
-                <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                />
-                <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                >
-                Sign In
-                </Button>
-            </Box>
+                <h1 component="h1" variant="h5">
+                    S'inscire
+                </h1>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    value={username}
+                    onChange={handleUsernameChange}
+                    autoComplete="username"
+                    autoFocus
+                    />
+                    <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    />
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    >
+                    Sign In
+                    </Button>
+                </Box>
             </Box>
         </>
     )
